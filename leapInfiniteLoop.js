b51faf7f42a7cb26cf9ecccbfe2c707b84877193
astupinski <astupins@uvm.edu>
2020-09-07 17:00:50 -0400

    getting data from leap motion

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,12 +1,16 @@
 var controllerOptions = {};
 
-i = 0;
 x = window.innerWidth/2;
 y = window.innerHeight/2;
 
 Leap.loop(controllerOptions, function(frame)
-{console.log(i++);
-var rand = Math.floor(Math.random() * (3) ) + -1;
-circle(x+rand,y,100);
+{
+if (frame.hands.length < 2 && frame.hands.length > 0) {
+	console.log(frame.hands[0]);
+}
+/*clear();
+var rand_up = Math.floor(Math.random() * (3) ) + -1;
+var rand_side = Math.floor(Math.random() * (3) ) + -1;
+circle(x+rand_side,y+rand_up,100);*/
 }
 );
