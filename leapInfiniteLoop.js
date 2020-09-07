ddc05a9babf76809d868e865081cbf880c407d8d
astupinski <astupins@uvm.edu>
2020-09-07 18:34:40 -0400

    refactoring code

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,24 +3,32 @@ var controllerOptions = {};
 x = window.innerWidth/2;
 y = window.innerHeight/2;
 
-Leap.loop(controllerOptions, function(frame)
-{
-//var hand = frame.hands[0];
-//console.log(hand.fingers);
 
-if (frame.hands.length < 2 && frame.hands.length > 0) {
-	var hand = frame.hands[0];
+function HandleFrame(frame) {
+	if (frame.hands.length < 2 && frame.hands.length > 0) {
+		var hand = frame.hands[0];
+		HandleHand(hand);
+	};
+	/*var rand_up = Math.floor(Math.random() * (3) ) + -1;
+	var rand_side = Math.floor(Math.random() * (3) ) + -1;
+	circle(x+rand_side,y+rand_up,100);*/
+};
+
+function HandleHand(hand) {
 	var fingers = hand.fingers;
-
 	hand.fingers.forEach(function(finger) {
-		if (finger.type == 1) {
-			console.log(finger);
-		};
+		HandleFinger(finger);
 	});
 };
-/*clear();
-var rand_up = Math.floor(Math.random() * (3) ) + -1;
-var rand_side = Math.floor(Math.random() * (3) ) + -1;
-circle(x+rand_side,y+rand_up,100);*/
-}
-);
+
+function HandleFinger(finger) {
+	if (finger.type == 1) {
+		console.log(finger);
+	};
+};
+
+Leap.loop(controllerOptions, function(frame) {
+	HandleFrame(frame);
+});
+
+
