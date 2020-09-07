0925bf71c6a4ae7650df0e34d519d81d6f90d57a
astupinski <astupins@uvm.edu>
2020-09-07 18:19:03 -0400

    getting index finger data

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,9 +5,19 @@ y = window.innerHeight/2;
 
 Leap.loop(controllerOptions, function(frame)
 {
+//var hand = frame.hands[0];
+//console.log(hand.fingers);
+
 if (frame.hands.length < 2 && frame.hands.length > 0) {
-	console.log(frame.hands[0]);
-}
+	var hand = frame.hands[0];
+	var fingers = hand.fingers;
+
+	hand.fingers.forEach(function(finger) {
+		if (finger.type == 1) {
+			console.log(finger);
+		};
+	});
+};
 /*clear();
 var rand_up = Math.floor(Math.random() * (3) ) + -1;
 var rand_side = Math.floor(Math.random() * (3) ) + -1;
