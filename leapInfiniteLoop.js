117045b7543dfbdf9509f5561ada084a6c58ae7a
astupinski <astupins@uvm.edu>
2020-09-07 22:07:37 -0400

    hw1 final product

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,8 +1,12 @@
 var controllerOptions = {};
 
-x = window.innerWidth/2;
-y = window.innerHeight/2;
+//x = window.innerWidth/2;
+//y = window.innerHeight/2;
 
+rawXMin = 100;
+rawXMax = 10;
+rawYMin = 100;
+rawYMax = 10;
 
 function HandleFrame(frame) {
 	if (frame.hands.length < 2 && frame.hands.length > 0) {
@@ -23,11 +27,43 @@ function HandleHand(hand) {
 
 function HandleFinger(finger) {
 	if (finger.type == 1) {
-		console.log(finger);
+		tip = finger.tipPosition;
+		x = tip[0];
+		y = tip[1];
+		z = tip[2];
+
+		if (x < rawXMin) {
+			rawXMin = x;	
+		};
+
+		if (x > rawXMax) {
+			rawXMax = x;
+		};
+
+		if (y < rawYMax) {
+			rawYMax = y;
+		};
+
+		if (y > rawYMin) {
+			rawYMin = y;
+		};
+
+		OldRangeX = (rawXMax - rawXMin);
+		NewRangeX = (window.innerWidth - 0); 		
+		NewValueX = (((x - rawXMin) * NewRangeX) / OldRangeX) + 0;
+
+		OldRangeY = (rawYMax - rawYMin);
+		NewRangeY = (window.innerHeight - 0);
+		NewValueY = (((y - rawYMin) * NewRangeY) / OldRangeY) + 0;
+
+		circle(NewValueX,NewValueY,100);
+		console.log(tip);
+		console.log(rawXMin, rawXMax, rawYMin, rawYMax);
 	};
 };
 
 Leap.loop(controllerOptions, function(frame) {
+	clear();
 	HandleFrame(frame);
 });
 
