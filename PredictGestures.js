cfd6b77f799eb4af85a2f3130956316c3a770440
astupinski <astupins@uvm.edu>
2020-11-23 12:37:19 -0500

    hw 10

--- PredictGestures.js
+++ PredictGestures.js
@@ -5,6 +5,8 @@ const knnClassifier = ml5.KNNClassifier();
 var trainingCompleted = false;
 var programState = 0;
 
+var digitToShow = 0;
+
 
 var numSamples = train5.shape[3];
 //var numFeatures = irisData.shape[1] - 1;
@@ -12,7 +14,10 @@ var predictedClassLabels = nj.zeros(numSamples);
 var framesOfData = nj.zeros([5,4,6]);
 var numPredictions = 0;
 var mean_acc = 0;
+var overall_acc = nj.zeros(2);
+var timeframe = 10;
 
+var timeSinceLastDigitChange = new Date();
 
 
 function HandleFrame(frame) {
@@ -97,27 +102,30 @@ function HandleBone(bone, type, hand_num, fingerIndex, boneIndex, ibox) {
 
     var canvasXnext = window.innerWidth/2 * normalizedNextJoint[0];
     var canvasYnext = window.innerHeight/2 * (1 - normalizedNextJoint[1]);
+
+    var greenVal = mean_acc * 255;
+    var redVal = (1-mean_acc) * 255;
     
 
     if (hand_num == 1) {
 
 	if (type == 0) {
-	    stroke(0,192,0,[192]);
+	    stroke(redVal,greenVal,0,[192]);
 	    strokeWeight(8*5);
 	};
 
 	if (type == 1) {
-	    stroke(0,129,0,[129]);
+	    stroke(redVal,greenVal,0,[129]);
 	    strokeWeight(6*5);
 	};
 
 	if (type == 2) {
-	    stroke(0,66,0,[66]);
+	    stroke(redVal,greenVal,0,[66]);
 	    strokeWeight(4*5);
 	};
 
 	if (type == 3) {
-	    stroke(0,10,0,[0]);
+	    stroke(redVal,greenVal,0,[0]);
 	    strokeWeight(2*5);
 	};
     };
@@ -157,101 +165,187 @@ function Train() {
 
 	features0 = train0.pick(null,null,null,i);
 	features02 = train0Bongard.pick(null,null,null,i);
+	features03 = train0Allison.pick(null,null,null,i);
+	features04 = train0Croxford.pick(null,null,null,i);
+	features05 = train0Davis.pick(null,null,null,i);
 	
 	features1 = train1.pick(null,null,null,i);
 	features12 = train1Bongard.pick(null,null,null,i);
+	features13 = train1Davis.pick(null,null,null,i);
+//	features14 = train1Hunt.pick(null,null,null,i);
+	features15 = train1Jimmo.pick(null,null,null,i);
 	
 	features2 = train2.pick(null,null,null,i);
 	features22 = train2Bongard.pick(null,null,null,i);
+	features23 = train2Banaszewski.pick(null,null,null,i);
+	features24 = train2Downs.pick(null,null,null,i);
+	features25 = train2Jing.pick(null,null,null,i);
 	
 	features3 = train3.pick(null,null,null,i);
 	features32 = train3Bongard.pick(null,null,null,i);
+	features33 = train3Beattie.pick(null,null,null,i);
+	features34 = train3Luksevish.pick(null,null,null,i);
+//	features35 = train3Nimako.pick(null,null,null,i);
 	
 	features4 = train4.pick(null,null,null,i);
 	features42 = train4Bongard.pick(null,null,null,i);
+	features43 = train4Beattie.pick(null,null,null,i);
+	features44 = train4Bertschinger.pick(null,null,null,i);
+	features45 = train4Faucher.pick(null,null,null,i);
 	
 	features5 = train5.pick(null,null,null,i);
 	features52 = train5Bongard.pick(null,null,null,i);
+	features53 = train5Bertschinger.pick(null,null,null,i);
+	features54 = train5Blewett.pick(null,null,null,i);
+	features55 = train5Faucher.pick(null,null,null,i);
 	
 	features6 = train6.pick(null,null,null,i);
 	features62 = train6Bongard.pick(null,null,null,i);
+	features63 = train6Fisher.pick(null,null,null,i);
+	features64 = train6Koretsky.pick(null,null,null,i);
+	features65 = train6Laquerre.pick(null,null,null,i);
 	
 	features7 = train7.pick(null,null,null,i);
 	features72 = train7Bongard.pick(null,null,null,i);
+	features73 = train7Fisher.pick(null,null,null,i);
+//	features74 = train7Gagnon.pick(null,null,null,i);
+	features75 = train7Laquerre.pick(null,null,null,i);
 	
 	features8 = train8.pick(null,null,null,i);
 	features82 = train8Bongard.pick(null,null,null,i);
+//	features83 = train8Gagnon.pick(null,null,null,i);
+	features84 = train8Goldman.pick(null,null,null,i);
+	features85 = train8ILee.pick(null,null,null,i);
 	
 	features9 = train9.pick(null,null,null,i);
 	features92 = train9Bongard.pick(null,null,null,i);
+	features93 = train9Croxford.pick(null,null,null,i);
+	features94 = train9JClark.pick(null,null,null,i);
+	features95 = train9McLaughlin.pick(null,null,null,i);
 	
 	
 	features0 = features0.reshape(1,120);
 	features02 = features02.reshape(1,120);
+	features03 = features03.reshape(1,120);
+	features04 = features04.reshape(1,120);
+	features05 = features05.reshape(1,120);
 	
 	features1 = features1.reshape(1,120);
 	features12 = features12.reshape(1,120);
+	features13 = features13.reshape(1,120);
+//	features14 = features14.reshape(1,120);
+	features15 = features15.reshape(1,120);
 	
 	features2 = features2.reshape(1,120);
 	features22 = features22.reshape(1,120);
+	features23 = features23.reshape(1,120);
+	features24 = features24.reshape(1,120);
+	features25 = features25.reshape(1,120);
 	
 	features3 = features3.reshape(1,120);
 	features32 = features32.reshape(1,120);
+	features33 = features33.reshape(1,120);
+	features34 = features34.reshape(1,120);
+//	features35 = features35.reshape(1,120);
 	
 	features4 = features4.reshape(1,120);
 	features42 = features42.reshape(1,120);
+	features43 = features43.reshape(1,120);
+	features44 = features44.reshape(1,120);
+	features45 = features45.reshape(1,120);
 	
 	features5 = features5.reshape(1,120);
 	features52 = features52.reshape(1,120);
+	features53 = features53.reshape(1,120);
+	features54 = features54.reshape(1,120);
+	features55 = features55.reshape(1,120);
 	
 	features6 = features6.reshape(1,120);
 	features62 = features62.reshape(1,120);
+	features63 = features63.reshape(1,120);
+	features64 = features64.reshape(1,120);
+	features65 = features65.reshape(1,120);
 	
 	features7 = features7.reshape(1,120);
 	features72 = features72.reshape(1,120);
+	features73 = features73.reshape(1,120);
+//	features74 = features74.reshape(1,120);
+	features75 = features75.reshape(1,120);
 	
 	features8 = features8.reshape(1,120);
 	features82 = features82.reshape(1,120);
+//	features83 = features83.reshape(1,120);
+	features84 = features84.reshape(1,120);
+	features85 = features85.reshape(1,120);
 	
 	features9 = features9.reshape(1,120);
 	features92 = features92.reshape(1,120);
+	features93 = features93.reshape(1,120);
+	features94 = features94.reshape(1,120);
+	features95 = features95.reshape(1,120);
 	
 	
 	knnClassifier.addExample(features0.tolist(), 0);
 	knnClassifier.addExample(features02.tolist(), 0);
+	knnClassifier.addExample(features03.tolist(), 0);
+	knnClassifier.addExample(features04.tolist(), 0);
+	knnClassifier.addExample(features05.tolist(), 0);
 	
 	knnClassifier.addExample(features1.tolist(), 1);
 	knnClassifier.addExample(features12.tolist(), 1);
+	knnClassifier.addExample(features13.tolist(), 1);
+//	knnClassifier.addExample(features14.tolist(), 1);
+	knnClassifier.addExample(features15.tolist(), 1);
 	
 	knnClassifier.addExample(features2.tolist(), 2);
 	knnClassifier.addExample(features22.tolist(), 2);
+	knnClassifier.addExample(features23.tolist(), 2);
+	knnClassifier.addExample(features24.tolist(), 2);
+	knnClassifier.addExample(features25.tolist(), 2);
+	
 	
 	knnClassifier.addExample(features3.tolist(), 3);
 	knnClassifier.addExample(features32.tolist(), 3);
+	knnClassifier.addExample(features33.tolist(), 3);
+	knnClassifier.addExample(features34.tolist(), 3);
+//	knnClassifier.addExample(features35.tolist(), 3);
 	
 	knnClassifier.addExample(features4.tolist(), 4);
 	knnClassifier.addExample(features42.tolist(), 4);
+	knnClassifier.addExample(features43.tolist(), 4);
+	knnClassifier.addExample(features44.tolist(), 4);
+	knnClassifier.addExample(features45.tolist(), 4);
 	
 	knnClassifier.addExample(features5.tolist(), 5);
 	knnClassifier.addExample(features52.tolist(), 5);
+	knnClassifier.addExample(features53.tolist(), 5);
+	knnClassifier.addExample(features54.tolist(), 5);
+	knnClassifier.addExample(features55.tolist(), 5);
 	
 	knnClassifier.addExample(features6.tolist(), 6);
 	knnClassifier.addExample(features62.tolist(), 6);
+	knnClassifier.addExample(features63.tolist(), 6);
+	knnClassifier.addExample(features64.tolist(), 6);
+	knnClassifier.addExample(features65.tolist(), 6);
 	
 	knnClassifier.addExample(features7.tolist(), 7);
 	knnClassifier.addExample(features72.tolist(), 7);
+	knnClassifier.addExample(features73.tolist(), 7);
+//	knnClassifier.addExample(features74.tolist(), 7);
+	knnClassifier.addExample(features75.tolist(), 7);
 	
 	knnClassifier.addExample(features8.tolist(), 8);
 	knnClassifier.addExample(features82.tolist(), 8);
+//	knnClassifier.addExample(features83.tolist(), 8);
+	knnClassifier.addExample(features84.tolist(), 8);
+	knnClassifier.addExample(features85.tolist(), 8);
 	
 	knnClassifier.addExample(features9.tolist(), 9);
 	knnClassifier.addExample(features92.tolist(), 9);
-
-
-
-
+	knnClassifier.addExample(features93.tolist(), 9);
+	knnClassifier.addExample(features94.tolist(), 9);
+	knnClassifier.addExample(features95.tolist(), 9);
 	
-
 	
     };
 };
@@ -279,10 +373,16 @@ function GotResults(err, result) {
     //console.log(parseInt(result.label));
     prediction = parseInt(result.label);
     numPredictions++;
-    var d = 9;
-    mean_acc = (((numPredictions-1)*mean_acc) + (prediction == d))/numPredictions;
+    mean_acc = (((numPredictions-1)*mean_acc) + (prediction == digitToShow))/numPredictions;
 
-    console.log(prediction);
+    if (mean_acc > overall_acc[digitToShow]) {
+	overall_acc[digitToShow] = mean_acc;
+	if (timeframe > 2) {
+	    timeframe = timeframe-3;
+	};
+    };
+
+    console.log(prediction, mean_acc);
 
     //console.log(predictedClassLabels.toString());
 
@@ -387,10 +487,10 @@ function DetermineState(frame) {
     hand_num = frame.hands.length;
     if (hand_num==0) {
 	programState = 0;
-    } else if (HandIsUncentered) {
-	programState = 1;
-    } else {
+    } else if (HandIsUncentered() == false) {
 	programState = 2;
+    } else if (HandIsUncentered() == true) {
+	programState = 1;
     };
 };
 
@@ -480,7 +580,7 @@ function HandleState0(frame) {
 function TrainKNNIfNotDoneYet() {
     if (trainingCompleted == false) {
 	
-	//Train();
+	Train();
 	trainingCompleted = true;
     };
 };
@@ -511,32 +611,142 @@ function HandleState1(frame) {
 };
 
 function DrawArrowRight() {
-    image(arrowRight, window.innerWidth/2, 0, window.innerWidth, window.innerHeight/2);
+    image(arrowRight, window.innerWidth/2, 0);
 };
 
 
 function DrawArrowLeft() {
-    image(arrowLeft, window.innerWidth/2, 0, window.innerWidth, window.innerHeight/2);
+    image(arrowLeft, window.innerWidth/2, 0);
 };
 
 function DrawArrowUp() {
-    image(arrowUp, window.innerWidth/2, 0, window.innerWidth, window.innerHeight/2);
+    image(arrowUp, window.innerWidth/2, 0);
 };
 
 function DrawArrowDown() {
-    image(arrowDown, window.innerWidth/2, 0, window.innerWidth, window.innerHeight/2);
+    image(arrowDown, window.innerWidth/2, 0);
 };
 
 function DrawArrowToward() {
-    image(arrowToward, window.innerWidth/2, 0, window.innerWidth, window.innerHeight/2);
+    image(arrowToward, window.innerWidth/2, 0);
 };
 
 function DrawArrowAway() {
-    image(arrowAway, window.innerWidth/2, 0, window.innerWidth, window.innerHeight/2);
+    image(arrowAway, window.innerWidth/2, 0);
 };
 
 function HandleState2(frame) {
     HandleFrame(frame);
+    DrawLowerRightPanel();
+    Test();
+    DetermineWhetherToDropImage();
+    DetermineWhetherToSwitchDigits();
+};
+
+function DrawLowerRightPanel() {
+    if (digitToShow == 0) {
+	image(signZero, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 1) {
+	image(signOne, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 2) {
+	image(signTwo, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 3) {
+	image(signThree, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 4) {
+	image(signFour, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 5) {
+	image(signFive, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 6) {
+	image(signSix, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 7) {
+	image(signSeven, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 8) {
+	image(signEight, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 9) {
+	image(signNine, window.innerWidth/2, window.innerHeight/2);
+	
+    };
+};
+
+
+function DrawNum() {
+    if (digitToShow == 0) {
+	image(signZeroNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 1) {
+	image(signOneNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 2) {
+	image(signTwoNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 3) {
+	image(signThreeNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 4) {
+	image(signFourNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 5) {
+	image(signFiveNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 6) {
+	image(signSixNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 7) {
+	image(signSevenNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 8) {
+	image(signEightNum, window.innerWidth/2, window.innerHeight/2);
+	
+    } else if (digitToShow == 9) {
+	image(signNineNum, window.innerWidth/2, window.innerHeight/2);
+	
+    };
+};
+
+
+function DetermineWhetherToDropImage() {
+    if (mean_acc > 0.3) {
+	DrawNum();
+    } else if (mean_acc < 0.3) {
+	DrawLowerRightPanel();
+    };
+};
+
+function DetermineWhetherToSwitchDigits() {
+    if (mean_acc > 0.5 && TimeToSwitchDigits() == true) {
+	SwitchDigits();
+	numPredictions = 0;
+    };
+};
+
+function SwitchDigits() {
+    digitToShow++;
+    if (digitToShow == 2) {
+	digitToShow = 0;
+    };
+    timeSinceLastDigitChange = new Date();
+};
+
+function TimeToSwitchDigits() {
+    var currentTime = new Date();
+    var timeInMilliseconds = currentTime - timeSinceLastDigitChange;
+    var timeInSeconds = timeInMilliseconds / 1000;
+    
+    if (timeInSeconds > timeframe) {
+	console.log("true");
+	return true;
+    } else {
+	return false;
+    };
 };
 
 
@@ -606,6 +816,9 @@ Leap.loop(controllerOptions, function(frame) {
 	
     } else if (programState==1) {
 	HandleState1(frame);
+	
+    } else if (programState==2) {
+	HandleState2(frame);
     };
  
     
--- prepareToDraw.js
+++ prepareToDraw.js
@@ -6,6 +6,29 @@ var arrowDown;
 var arrowToward;
 var arrowAway;
 
+var signZero;
+var signOne;
+var signTwo;
+var signThree;
+var signFour;
+var signFive;
+var signSix;
+var signSeven;
+var signEight;
+var signNine;
+
+var signZeroNum;
+var signOneNum;
+var signTwoNum;
+var signThreeNum;
+var signFourNum;
+var signFiveNum;
+var signSixNum;
+var signSevenNum;
+var signEightNum;
+var signNineNum;
+
+
 function setup() { 
     createCanvas(window.innerWidth,window.innerHeight);
     img = loadImage('https://i.imgur.com/5yjaFH7.png');
@@ -15,4 +38,30 @@ function setup() {
     arrowDown = loadImage('https://i.imgur.com/gkcAn28.png');
     arrowToward = loadImage('https://i.imgur.com/0SbadkN.png');
     arrowAway = loadImage('https://i.imgur.com/yThzf7t.png');
+
+    signZero = loadImage('https://i.imgur.com/xP69744.png');
+    signOne = loadImage('https://i.imgur.com/NY4LMxD.png');
+    signTwo = loadImage('https://i.imgur.com/ccIaNoD.png');
+    signThree = loadImage('https://i.imgur.com/lSCxSYB.png');
+    signFour = loadImage('https://i.imgur.com/fagCyzb.png');
+    signFive = loadImage('https://i.imgur.com/PHUVKzQ.png');
+    signSix = loadImage('https://i.imgur.com/qNNAt2N.png');
+    signSeven = loadImage('https://i.imgur.com/30vWpU8.png');
+    signEight = loadImage('https://i.imgur.com/pBsYxpa.png');
+    signNine = loadImage('https://i.imgur.com/JGzr1OW.png');
+
+
+    signZeroNum = loadImage('https://i.imgur.com/kZFUoZM.png');
+    signOneNum = loadImage('https://i.imgur.com/RrLweKZ.png');
+    signTwoNum = loadImage('https://i.imgur.com/Iqbqh7b.png');
+    signThreeNum = loadImage('https://i.imgur.com/oeIV01w.png');
+    signFourNum = loadImage('https://i.imgur.com/z5zvp94.png');
+    signFiveNum = loadImage('https://i.imgur.com/X1WzYek.png');
+    signSixNum = loadImage('https://i.imgur.com/KePYNcN.png');
+    signSevenNum = loadImage('https://i.imgur.com/flzOk7d.png');
+    signEightNum = loadImage('https://i.imgur.com/VHo0wFQ.png');
+    signNineNum = loadImage('https://i.imgur.com/VV8ZrNQ.png');
+
+    
 }
+
